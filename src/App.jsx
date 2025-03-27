c30d2225413d76b23483217cfe0dab97b7ceeea4
Hamed <h.namazi91@gmail.com>
2025-03-27 14:34:54 +0430

    fetching subreddit data

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import React, { Component } from "react";
 import Subreddit from "./assets/components/subreddit";
+import DataContext from "./assets/context/dataContext.jsx";
 
 class App extends Component {
   render() {
     return (
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col border border-dark">
-            <Subreddit />
-          </div>
-          <div className="col border border-dark">
-            <Subreddit />
-          </div>
-          <div className="col border border-dark">
-            <Subreddit />
+      <DataContext.Provider value={
+       { setData: this.setData}
+      }>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col border border-dark">
+              <Subreddit />
+            </div>
+            <div className="col border border-dark">
+              <Subreddit />
+            </div>
+            <div className="col border border-dark">
+              <Subreddit />
+            </div>
           </div>
         </div>
-      </div>
+      </DataContext.Provider>
     );
   }
 }
--- src/assets/components/subreddit.jsx
+++ src/assets/components/subreddit.jsx
@@ -1,4 +1,5 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState} from "react";
+
 
 const Subreddit = () => {
   const [inputValue, setInputValue] = useState("");
@@ -13,18 +14,50 @@ const Subreddit = () => {
   const closeDialog = (e) => {
     e.preventDefault();
     if (dialogRef.current) {
-      setInputValue("")
+      setInputValue("");
       dialogRef.current.close();
     }
   };
-  
-  const handleInputChange = (e)=>{
-    setInputValue(e.target.value)
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const fetchReddit = async (e) => {
+    e.preventDefault();
+
+  const saveData = (data)=>{
+    console.log(data.data.children)
+
+
   }
 
-  const fetchReddit = (e) => {
-    e.preventDefault();
-    console.log(inputValue)
+    const authCode =
+      "eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjpzS3dsMnlsV0VtMjVmcXhwTU40cWY4MXE2OWFFdWFyMnpLMUdhVGxjdWNZIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNzQzMTUxNzYxLjUxNjM2MiwiaWF0IjoxNzQzMDY1MzYxLjUxNjM2MSwianRpIjoiZExBMUtfNkN2TFRUeW4zWnUtcjFWZmxnQVg1ZFVBIiwiY2lkIjoiam5BZ0NuU0JKazhxdzROWEUxZXRaZyIsImxpZCI6InQyXzFtMXc1dWVmNmUiLCJhaWQiOiJ0Ml8xbTF3NXVlZjZlIiwibGNhIjoxNzQzMDY0OTUwNjA1LCJzY3AiOiJlSnlLVnRKU2lnVUVBQURfX3dOekFTYyIsImZsbyI6OX0.lCRUC7wBJ3yYjV403mo5o4st2IBe5RQQAGOYJwJY6irtttG8hdnpfwOHj1TOhX88djsoCRkZjhTZF_zj_3EJ7kVLEe9848mpbxlRQ0h-8ackj3C_kh0mbpGMjoy9HIJ_HoG8nQYMypz9Y2ftSgKlVCq4a-roBmo8VI2c7pRDlLqt96Inl3yvb_PQGeLTg0e5ft_f1rIeXVcvqpL3Ur0c6-m8X_k1X6D7DUpvNatB8bvdnODY3o3ftem6RoD4GvHNlyMFoF9Br36LStuxmbR_O05FSQv5oN81_7mTVx3qxJVnH1QxuOBrySbvh9pT7repxAoAqd9wsSj0YR3BIK3lyg";
+
+    try {
+      const response = await fetch(
+        `https://oauth.reddit.com/r/${inputValue}/.json`,
+        {
+          method: "GET",
+          headers: {
+            'Authorization': `Bearer ${authCode}`,
+            "User-Agent": "client/1.0 (by hamed-namazi)",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      data.kind
+        ? saveData(data)
+        : alert("Wrong subreddit name or restricted subreddit!");
+    } catch (err) {
+      console.error("Error fetching subreddit data:", err);
+    }
   };
 
   return (
